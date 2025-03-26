349fa18facb0497621434fde9bff174959b647ca
stoff <christoph.tiger@hotmail.com>
2025-03-26 12:44:56 +0100

    introduce variables

--- frontend/src/components/fiat/TransactionCards.tsx
+++ frontend/src/components/fiat/TransactionCards.tsx
@@ -7,7 +7,6 @@ import {useFetchNetWorth} from "../../hooks/useFetchNetWorth";
 const TransactionCards = ({ approvedSum, transactions }) => {
   const { netWorth } = useFetchNetWorth();
 
-
   const totalXmrWithdrawals = transactions
       .filter((tx) => tx.type.toLowerCase() === "withdrawal")
       .reduce((sum, tx) => sum + (parseFloat(tx.chf_value) || 0), 0);
@@ -22,29 +21,38 @@ const TransactionCards = ({ approvedSum, transactions }) => {
 
   const totalFees = transactions.reduce((sum, tx) => sum + (parseFloat(tx.fee) || 0), 0);
 
+  const staticData = {
+  coinbaseWithdrawals: 1460,
+  weedWithdrawals: 6000,
+  initialDeposit: 6715.0
+  }
+  const lastNetWorth = (netWorth?.[netWorth.length - 1]?.totalNetWorth || 0) * 0.9;
+
+  const netWithdrawals = totalWithdrawals - staticData.coinbaseWithdrawals - staticData.weedWithdrawals - approvedSum - totalXmrWithdrawals;
+  const totalDepositsWithInitial = totalDeposits + staticData.initialDeposit;
+  const netProfit = totalWithdrawals + totalDepositsWithInitial - staticData.coinbaseWithdrawals - staticData.weedWithdrawals - approvedSum - totalXmrWithdrawals - lastNetWorth;
 
   return (<Container sx={{ display: {md: "flex" }, justifyContent: "space-between", marginBottom: 5 }}>
       <Card sx={{ padding: 3, borderRadius: 10, marginY: 3 }}>
         <Typography variant="h5">Deposits</Typography>
         <Typography variant="h4" fontWeight="bold">
-          CHF {toFixedString(totalDeposits + 6715.0, 0)}
+          CHF {toFixedString(totalDepositsWithInitial, 0)}
         </Typography>
       </Card>
-
 
     <Tooltip title={
       <Box>
         <Typography variant="body2">gnosis {toFixedString(approvedSum, 0)} CHF</Typography>
         <Typography variant="body2">kraken {toFixedString(totalWithdrawals, 0)} CHF</Typography>
         <Typography variant="body2">kraken xmr {toFixedString(totalXmrWithdrawals, 0)} CHF</Typography>
-        <Typography variant="body2">coinbase 1460 CHF</Typography>
-        <Typography variant="body2">weed ca 6000 CHF</Typography>
+        <Typography variant="body2">coinbase ${staticData.coinbaseWithdrawals} CHF</Typography>
+        <Typography variant="body2">weed ca ${staticData.weedWithdrawals} CHF</Typography>
       </Box>
     } arrow>
         <Card sx={{ padding: 3, borderRadius: 10, marginY: 3 }}>
           <Typography variant="h5">Withdrawals</Typography>
           <Typography variant="h4" fontWeight="bold">
-            CHF {toFixedString(totalWithdrawals - 1460 - 6000 - approvedSum - totalXmrWithdrawals, 0)}
+            CHF {toFixedString(netWithdrawals, 0)}
           </Typography>
         </Card>
       </Tooltip>
@@ -59,7 +67,7 @@ const TransactionCards = ({ approvedSum, transactions }) => {
       <Card sx={{ padding: 3, borderRadius: 10, marginY: 3 }}>
         <Typography variant="h5">Net Profit</Typography>
         <Typography variant="h4" fontWeight="bold">
-          CHF {toFixedString(totalWithdrawals + totalDeposits + 6715.0 - 1460 - 6000 - approvedSum - totalXmrWithdrawals - (netWorth?.[netWorth.length - 1]?.totalNetWorth || 0) * 0.9 , 0)}
+          CHF {toFixedString(netProfit, 0)}
         </Typography>
       </Card>
 
